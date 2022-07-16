8f016e163b5d03680792d05b0bf20bd107889056
kim <ing03201@gmail.com>
2022-07-16 12:02:30 +0900

    boj1541 수정
    단순 문자열 탐색으로 풀면 해결될거같아서 씁니다

--- 7주차/Greedy/1541.cpp
+++ 7주차/Greedy/1541.cpp
@@ -1,107 +1,38 @@
 #include <iostream>
-#include <stack>
 #include <vector>
 #include <string>
-#include <queue>
 
 using namespace std;
-using std::stoi;
 
 /*
-
 문자열 중에 - 가 있으면
 마이너스를 만나기 전, 후 숫자들을 분리해서
 before = 마이너스를 만나기 전 숫자들의 합
 after = 마이너스 이후의 숫자들의 합
-
 before - after = 정답
-
 */
 
-void min(string text);
-
-int main()
-{
-
-    string text;
-    cin >> text;
-    min(text);
+int main(int argc, char** argv){
+    std:string input;
+    int num = 0; // input에서  입력할 숫자들
+    int answer = 0; // 리턴할 답
+    int flag = 1; // 양수 음수 부호를 결정하는 변수
+    cin >> input;
+    for(char c : input){
+        if(c == '+'){
+            answer += num;
+            num = 0;
+        }
+        else if(c=='-'){
+            flag = -1;
+            answer += num;
+            num = 0;
+        }
+        else{
+            num = num * 10 + (c - '0')*flag;
+        }
+    }
+    answer += num;
+    cout << answer << endl;
+    return 0;
 }
-
-void min(string text)
-{
-    string str;
-    stack<int> number;
-    queue<char> mix;
-
-    int result;
-
-    for (int i = 0; i < text.length(); i++)
-    {
-        mix.push(text[i]);
-    };
-
-    while (!mix.empty())
-    {
-        char temp;
-        temp = mix.front();
-        mix.pop();
-
-        // str += temp;
-
-        if (temp != '+' || temp != '-')
-        {
-            str += temp;
-        }
-
-        if (temp == '+')
-        {
-            int num = stoi(str);
-            number.push(num);
-            number.push(0);
-            str = "";
-        };
-
-        if (temp == '-')
-        {
-            int num = stoi(str);
-            number.push(num);
-            number.push(-1);
-            str = "";
-        };
-
-        if (mix.size() == 0)
-        {
-            int num = stoi(str);
-            number.push(num);
-            str = "";
-        }
-    };
-
-    while (!number.empty())
-    {
-        int cur = number.top();
-        number.pop();
-        if (cur == -1)
-        {
-            result = result * -1;
-        }
-        else
-        {
-            result += cur;
-            // cout <<"result: " << result << endl;
-        }
-    };
-
-    cout << result << endl;
-};
-
-/*
-
-
-제출 시 오류가 나는데 왜 나는지 이해가 안감
-
-
-
-
-*/
