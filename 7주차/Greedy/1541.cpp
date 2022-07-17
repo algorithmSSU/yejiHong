ef9891928660162a43d72bbd0f30fff4239935ea
yeji <yeji3852@daum.net>
2022-07-17 20:55:16 +0900

    7주차

--- 7주차/Greedy/1541.cpp
+++ 7주차/Greedy/1541.cpp
@@ -1,107 +1,141 @@
 #include <iostream>
-#include <stack>
-#include <vector>
 #include <string>
+#include <vector>
 #include <queue>
 
 using namespace std;
+using std::invalid_argument;
 using std::stoi;
 
-/*
-
-문자열 중에 - 가 있으면
-마이너스를 만나기 전, 후 숫자들을 분리해서
-before = 마이너스를 만나기 전 숫자들의 합
-after = 마이너스 이후의 숫자들의 합
-
-before - after = 정답
-
-*/
-
-void min(string text);
-
+void min(string t);
+bool check(string t);
 int main()
 {
 
     string text;
     cin >> text;
+
     min(text);
-}
-
-void min(string text)
-{
-    string str;
-    stack<int> number;
-    queue<char> mix;
-
-    int result;
-
-    for (int i = 0; i < text.length(); i++)
-    {
-        mix.push(text[i]);
-    };
-
-    while (!mix.empty())
-    {
-        char temp;
-        temp = mix.front();
-        mix.pop();
-
-        // str += temp;
-
-        if (temp != '+' || temp != '-')
-        {
-            str += temp;
-        }
-
-        if (temp == '+')
-        {
-            int num = stoi(str);
-            number.push(num);
-            number.push(0);
-            str = "";
-        };
-
-        if (temp == '-')
-        {
-            int num = stoi(str);
-            number.push(num);
-            number.push(-1);
-            str = "";
-        };
-
-        if (mix.size() == 0)
-        {
-            int num = stoi(str);
-            number.push(num);
-            str = "";
-        }
-    };
-
-    while (!number.empty())
-    {
-        int cur = number.top();
-        number.pop();
-        if (cur == -1)
-        {
-            result = result * -1;
-        }
-        else
-        {
-            result += cur;
-            // cout <<"result: " << result << endl;
-        }
-    };
-
-    cout << result << endl;
 };
 
-/*
+bool check(string t)
+{
+    for (int i = 0; i < t.length(); i++)
+    {
+        if (t[i] == '-')
+        {
+            break;
+        };
+        if (i == t.length() - 1 && t[i] != '-')
+        {
+            return false;
+        };
+    };
+    return true;
+};
 
+void min(string t)
+{
+    int front, back;
+    int front_sum = 0;
+    int back_sum = 0;
+    int start = 0;
+    int result;
+    int index;
+    int answer;
+    string str;
+    string front_str, back_str;
+    vector<int> num_list;
 
-제출 시 오류가 나는데 왜 나는지 이해가 안감
+    if (check(t) == true)
+    {
+        // - index 찾기
+        for (int i = 0; i < t.length(); i++)
+        {
+            if (t[i] == '-')
+            {
+                index = i;
+                break;
+            };
+        };
 
+        // cout << "index:" << index << endl;
 
+        /*front*/
+        for (int i = 0; i <= index; i++)
+        {
+            char cur = t[i];
 
+            // cout << cur << endl;
 
-*/
+            if (cur != '+' || cur != '-')
+            {
+                str += cur;
+            };
+            if (cur == '+' || cur == '-')
+            {
+                int num = stoi(str);
+                front_sum += num;
+                str = "";
+            };
+        };
+        // cout << front_sum << endl;
+        str = "";
+
+        /*back*/
+        for (int j = index + 1; j < t.length(); j++)
+        {
+            char cur = t[j];
+
+            // cout << cur << endl;
+
+            if (cur != '+' || cur != '-')
+            {
+                str += cur;
+            };
+            if (cur == '+' || cur == '-')
+            {
+                int num = stoi(str);
+                back_sum += num;
+                str = "";
+            };
+            if (j == t.length() - 1)
+            {
+                int num = stoi(str);
+                back_sum += num;
+                str = "";
+            }
+        };
+        cout << front_sum - back_sum << endl;
+        return;
+    };
+
+    if (check(t) == false)
+    {
+        for (int i = 0; i < t.length(); i++)
+        {
+            char cur = t[i];
+            if (cur != '+' || cur != '-')
+            {
+                str += cur;
+            };
+            if (cur == '+' || cur == '-')
+            {
+                int num = stoi(str);
+                back_sum += num;
+                str = "";
+            };
+            if (i == t.length() - 1)
+            {
+                int num = stoi(str);
+                back_sum += num;
+                str = "";
+            };
+        };
+
+        cout << back_sum << endl;
+        return;
+    };
+
+    // cout << "back_sum:"<< back_sum << endl;
+};
