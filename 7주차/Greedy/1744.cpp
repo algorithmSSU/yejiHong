107ae3223fca681d6d37fb532cbabf7aa2fdf791
yeji <yeji3852@daum.net>
2022-07-20 03:30:20 +0900

    8주차

--- 7주차/Greedy/1744.cpp
+++ 7주차/Greedy/1744.cpp
@@ -17,21 +17,29 @@ int solution();
 
 int p_size, n_size, z_size;
 
-int main(){ 
-    
-    
+int main()
+{
+
     cin >> n;
 
-    for (int i = 0; i < n;i++){
+    for (int i = 0; i < n; i++)
+    {
         int temp;
         cin >> temp;
-        if(temp>1){
+        if (temp > 1)
+        {
             positive.push_back(temp);
-        }else if(temp==1){
+        }
+        else if (temp == 1)
+        {
             result += 1;
-        }else if(temp == 0){
+        }
+        else if (temp == 0)
+        {
             zero_count += 1;
-        }else{
+        }
+        else
+        {
             negative.push_back(temp);
         }
     };
@@ -39,24 +47,30 @@ int main(){
     cout << solution() << endl;
 };
 
-int solution(){
+int solution()
+{
     sort(positive.begin(), positive.end(), greater<int>());
     sort(negative.begin(), negative.end(), less<int>());
 
-    for (int i = 0; i < positive.size()-(positive.size()%2);i+=2){
+    for (int i = 0; i < positive.size() - (positive.size() % 2); i += 2)
+    {
         result += positive[i] * positive[i + 1];
     };
 
-    if(positive.size()%2==1){
+    if (positive.size() % 2 == 1)
+    {
         result += positive.back();
     };
 
-    for (int i = 0; i < negative.size() - (negative.size()%2);i+=2){
+    for (int i = 0; i < negative.size() - (negative.size() % 2); i += 2)
+    {
         result += negative[i] * negative[i + 1];
     };
 
-    if(negative.size()%2==1){
-        if(zero_count==0){
+    if (negative.size() % 2 == 1)
+    {
+        if (zero_count == 0)
+        {
             result += negative.back();
         };
     };
