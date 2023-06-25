2721af859e27cdef0b595b7e841d1980efda9115
yj120 <yeji3852@daum.net>
2023-06-26 00:45:43 +0900

    daily_solving

--- Weekly problem solving/June/programmers/모의고사.cpp
+++ Weekly problem solving/June/programmers/모의고사.cpp
@@ -1,87 +1,57 @@
-#include <string>
+
+#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <limits>
+#include <climits>
+
 using namespace std;
-
-vector<int> cal_a(vector<int> v, int s)
-{
-    vector<int> pick;
-    int start = 1;
-    while (s--)
-    {
-        if (start <= 5)
-        {
-            pick.emplace_back(start);
-            start++;
-        }
-        else
-        {
-            start = 1;
-        }
-    }
-    return pick;
-};
-vector<int> cal_b(vector<int> v, int s)
-{
-    vector<int> pick = {2, 1, 2, 3, 4, 2, 5, 2};
-    vector<int> ans;
-    int start_idx = 0;
-
-    while (s--)
-    {
-        if (start_idx <= pick.size())
-        {
-            ans.emplace_back(pick[start_idx]);
-            start_idx++;
-        }
-        else
-        {
-            start_idx = 0;
-        }
-    }
-    return ans;
-};
-
-vector<int> cal_c(vector<int> v, int s)
-{
-    vector<int> pick = {3, 3, 1, 1, 4, 4, 5, 5};
-    int start_idx = 0;
-    vector<int> ans;
-    while (s--)
-    {
-        if (start_idx <= pick.size())
-        {
-            ans.emplace_back(pick[start_idx]);
-            start_idx++;
-        }
-        else
-        {
-            start_idx = 0;
-        }
-    }
-    return ans;
-}
 
 vector<int> solution(vector<int> answers)
 {
     vector<int> answer;
-    int size = answers.size();
-    vector<int> a;
-    vector<int> b;
-    vector<int> c;
+    vector<vector<int>> pick{{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+    int ans[3] = {0, 0, 0};
 
-    a = cal_a(answers, size);
-    b = cal_b(answers, size);
-    c = cal_c(answers, size);
+    for (int i = 0; i < answers.size(); i++)
+    {
+        if (answers[i] == pick[0][i % 5])
+        {
+            ans[0]++;
+        };
+        if (answers[i] == pick[1][i % 8])
+        {
+            ans[1]++;
+        };
+        if (answers[i] == pick[2][i % 10])
+        {
+            ans[2]++;
+        };
+    };
 
-    return a;
+    int maxv = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        maxv = maxv < ans[i] ? ans[i] : maxv;
+    };
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (maxv == ans[i])
+        {
+            answer.emplace_back(i + 1);
+        };
+    }
+
+    return answer;
 };
 
 int main(){
 
-    vector<int>answer;
-    vector<int> v ={1,2,3,4,5};
-    answer = solution(v)
-    
-    
-}
+    vector<int> answers{0,1,2};
+    vector<int> res;
+    res= solution(answers);
+
+    return 0;
+
+};
