ea13ec5f62d60ffa3dbe86dc21c291300e5c4502
yj120 <yeji3852@daum.net>
2023-07-15 02:30:01 +0900

    daily_solving

---
+++ Weekly problem solving/July/baekjoon/combination/6603_로또.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    vector<int> v(4);
+    
+
+    // 1부터 4까지 벡터에 저장
+    for (int i = 0; i < 4; i++)
+    {
+        v[i] = i + 1;
+    }
+
+    
+    
+}
--- Weekly problem solving/July/baekjoon/sequence/2003_수들의합2.cpp
+++ Weekly problem solving/July/baekjoon/sequence/2003_수들의합2.cpp
@@ -1,40 +1,37 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
 int main(){
+    int num[100001];
     int n,m;
-    int arr[10001];
     cin >> n >> m;
 
-    for(int i=1;i<=n;i++){
-        cin >> arr[i];
+    for(int i=0;i<n;i++){
+        cin >> num[i];
     };
 
-    int save[10002];
-
-
+    // two pointer
+    int startidx = 0;
+    int endidx = 0;
+    int sum = 0;
     int cnt = 0;
-    for(int i=0;i<=n;i++){
-        save[i+1] = arr[i+1] + save[i];
-        if(arr[i]==m || save[i+1]==m){
-            cnt++;
-        }else{
-            if(save[i+1]>m){
-                int temp = save[i+1]-save[i]+arr[i];
-                if(temp==m){
-                    cnt++;
-                }
-            }else if(save[i+1]<m){
-                save[i+1]=save[i+1]-save[i]+arr[i];
 
-            }
+    while(endidx<=n){
+        if(sum<m){
+            sum+=num[endidx];
+            endidx++;
+
         };
-    };
+        if(sum>=m){
+            if(sum==m){
+                cnt++;
+            }
+            sum-=num[startidx];
+            startidx++;
+        }
+    }
 
-    cout << cnt << endl;
-    
+    cout << cnt << '\n';
 
-    
+
 }
