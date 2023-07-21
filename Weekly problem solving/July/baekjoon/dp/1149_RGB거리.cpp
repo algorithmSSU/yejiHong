2151055cbf811d9262171f49bd1cbf13fabf41af
yj120 <yeji3852@daum.net>
2023-07-21 20:14:24 +0900

    daily_solving

---
+++ Weekly problem solving/July/baekjoon/dp/1149_RGB거리.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int load[101][3];
+
+dp[101][3]={0,};
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<3;j++){
+            cin >> load[i][j];
+        }
+    }
+
+    
+
+
+
+}
