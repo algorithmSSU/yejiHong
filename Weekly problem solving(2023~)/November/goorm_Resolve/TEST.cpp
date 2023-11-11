4f85d9651858b3801d9f5ec9662de1be9b48f99f
yj120 <yeji3852@daum.net>
2023-11-11 17:46:58 +0900

    daily_solving

---
+++ Weekly problem solving(2023~)/November/goorm_Resolve/TEST.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+  string s = "abcde";
+
+  auto it = remove(s.begin(),s.end(),'a');
+  
+  
+  
+
+  
+}
